11d8dba70079177866e7a5d39835bd587e4c3beb
Tatyana404 <kot.kate2020@gmail.com>
2022-06-05 01:17:21 +0300

    feat: add function list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,23 @@
+import { readdir } from 'fs/promises'
+
 export const list = async () => {
-    // Write your code here 
-};
+  try {
+    const fileNames = await readdir('./files')
+    const result = []
+
+    for (const fileName of fileNames) {
+      result.push(fileName)
+    }
+
+    console.log(result)
+  } catch (err) {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        throw new Error('FS operation failed')
+      }
+      throw err
+    }
+  }
+}
+
+await list()
