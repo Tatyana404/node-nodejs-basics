941bd8b08962a2d8c84038d249051280fe1fc210
Tatyana404 <kot.kate2020@gmail.com>
2022-06-04 20:15:47 +0300

    feat: add function decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,22 @@
+import { createReadStream, createWriteStream } from 'fs'
+import { createUnzip } from 'zlib'
+import { pipeline } from 'stream'
+import { promisify } from 'util'
+
 export const decompress = async () => {
-    // Write your code here 
-};
+  try {
+    const inp = createReadStream('./archive.gz')
+    const out = createWriteStream('./files/fileToCompress.txt')
+    const pipe = promisify(pipeline)
+    const unzip = createUnzip()
+
+    await pipe(inp, unzip, out)
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      throw new Error('Error while unpacking archive')
+    }
+    throw err
+  }
+}
+
+await decompress()
