bb33753d9137625a8c3d6b25f783bf18530970d2
Tatyana404 <kot.kate2020@gmail.com>
2022-06-04 22:07:42 +0300

    refactor: open module rewritten to asynchronous

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,19 +1,16 @@
-import { writeFile } from 'fs/promises'
-import { open } from 'fs'
+import { open, writeFile } from 'fs/promises'
 
 export const create = async () => {
   try {
-    open('files/fresh.txt', 'wx', err => {
-      if (err) {
-        if (err.code === 'EEXIST') {
-          throw new Error('FS operation failed')
-        }
-        throw err
-      }
-    })
+    await open('files/fresh.txt', 'wx')
     await writeFile('files/fresh.txt', 'I am fresh and young')
   } catch (err) {
-    throw err
+    if (err) {
+      if (err.code === 'EEXIST') {
+        throw new Error('FS operation failed')
+      }
+      throw err
+    }
   }
 }
 
