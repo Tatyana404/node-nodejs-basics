5127bcd54ab40e615374394465d57137ef959dcb
Tatyana404 <kot.kate2020@gmail.com>
2022-06-04 20:15:12 +0300

    feat: add function compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,22 @@
+import { createReadStream, createWriteStream } from 'fs'
+import { createGzip } from 'zlib'
+import { pipeline } from 'stream'
+import { promisify } from 'util'
+
 export const compress = async () => {
-    // Write your code here 
-};
+  try {
+    const inp = createReadStream('./files/fileToCompress.txt')
+    const out = createWriteStream('archive.gz')
+    const pipe = promisify(pipeline)
+    const gzip = createGzip()
+
+    await pipe(inp, gzip, out)
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      throw new Error('Error creating archive')
+    }
+    throw err
+  }
+}
+
+await compress()
