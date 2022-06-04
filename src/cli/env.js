86424c8a175376eb0f05c4fec1ac154c7e6c3236
Tatyana404 <kot.kate2020@gmail.com>
2022-06-04 20:13:59 +0300

    feat: add function parseEnv

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,15 @@
 export const parseEnv = () => {
-    // Write your code here 
-};
+  const env = process.env
+  const keys = Object.keys(env)
+  const values = Object.values(env)
+  let count = 0
+
+  for (let i = 0; i < keys.length; i++) {
+    if (keys[i].substring(0, 4) === 'RSS_') {
+      count++
+      console.log(`${keys[i]}${count}=${values[i]}${count};`)
+    }
+  }
+}
+
+parseEnv()
