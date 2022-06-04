1c2db9389c4b04275d7d65192508cfd2fbb44605
Tatyana404 <kot.kate2020@gmail.com>
2022-06-05 01:05:48 +0300

    feat: add function read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,18 @@
+import { readFile } from 'fs/promises'
+
 export const read = async () => {
-    // Write your code here 
-};
+  try {
+    const fileContent = await readFile('files/fileToRead.txt')
+
+    console.log(fileContent.toString())
+  } catch (err) {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        throw new Error('FS operation failed')
+      }
+      throw err
+    }
+  }
+}
+
+await read()
