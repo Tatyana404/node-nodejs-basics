3b2e9d33950e0116ac76ee94ee6336a7aa239168
Tatyana404 <kot.kate2020@gmail.com>
2022-06-04 20:17:03 +0300

    feat: add function remove

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,16 @@
+import { unlink } from 'fs/promises'
+
 export const remove = async () => {
-    // Write your code here 
-};
+  try {
+    await unlink('files/fileToRemove.txt')
+  } catch (err) {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        throw new Error('FS operation failed')
+      }
+      throw err
+    }
+  }
+}
+
+await remove()
