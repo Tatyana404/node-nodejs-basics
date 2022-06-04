5b84c6036b0296db385b57194c303562884cdcda
Tatyana404 <kot.kate2020@gmail.com>
2022-06-05 02:05:13 +0300

    feat: add function rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,16 @@
+import { rename as renameFile } from 'fs/promises'
+
 export const rename = async () => {
-    // Write your code here 
-};
+  try {
+    await renameFile('./files/wrongFilename.txt', './files/properFilename.md')
+  } catch (err) {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        throw new Error('FS operation failed')
+      }
+      throw err
+    }
+  }
+}
+
+await rename()
