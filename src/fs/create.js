9c3f3a2ef500c4dc59928cb2afd26e897363aec3
Tatyana404 <kot.kate2020@gmail.com>
2022-06-04 20:16:18 +0300

    feat: add function create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,20 @@
+import { writeFile } from 'fs/promises'
+import { open } from 'fs'
+
 export const create = async () => {
-    // Write your code here 
-};
+  try {
+    open('files/fresh.txt', 'wx', err => {
+      if (err) {
+        if (err.code === 'EEXIST') {
+          throw new Error('FS operation failed')
+        }
+        throw err
+      }
+    })
+    await writeFile('files/fresh.txt', 'I am fresh and young')
+  } catch (err) {
+    throw err
+  }
+}
+
+await create()
