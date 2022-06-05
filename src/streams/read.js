e4b99939e3780602f0a5403efc8ed83387aec02e
Tatyana404 <kot.kate2020@gmail.com>
2022-06-05 21:21:46 +0300

    feat: add function read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,7 @@
-export const read = async () => {
-    // Write your code here 
-};
+import { createReadStream } from 'fs'
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
+
+export const read = async () => createReadStream(join(dirname(fileURLToPath(import.meta.url)), './files/fileToRead.txt')).pipe(process.stdout)
+
+await read()
