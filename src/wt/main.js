dca0268630a8b690bb9c3978282bd921897f1905
Tatyana404 <kot.kate2020@gmail.com>
2022-06-05 21:02:58 +0300

    feat: add function performCalculations

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,39 @@
+import { Worker } from 'worker_threads'
+import { cpus } from 'os'
+
 export const performCalculations = async () => {
-    // Write your code here
-};
+  const result = []
+
+  const createWorker = workerData =>
+    new Promise((response, reject) => {
+      const workers = new Worker('./worker.js', { workerData })
+
+      workers.once('message', res => {
+        response({
+          status: 'resolved',
+          data: res
+        })
+      })
+
+      workers.on('error', err => {
+        console.error(err)
+      })
+
+      workers.on('exit', code => {
+        if (code !== 0) {
+          response({
+            status: 'error',
+            data: null
+          })
+        }
+      })
+    })
+
+  for (let i = 0; i < cpus().length; i++) {
+    result[i] = createWorker(i + 10)
+  }
+
+  console.log((await Promise.allSettled(result)).map(item => item.value))
+}
+
+await performCalculations()
