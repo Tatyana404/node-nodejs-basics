f00cd35a2a0045001490c96391a27c6bffbb85d8
Tatyana404 <kot.kate2020@gmail.com>
2022-06-05 15:23:41 +0300

    refactor: add a line break

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -3,7 +3,7 @@ import { Transform } from 'stream'
 export const transform = async () => {
   const reverseText = new Transform({
     transform (chunk, encoding, callback) {
-      callback(null, chunk.toString().split('').reverse().join(''))
+      callback(null, `${chunk.toString().split('').reverse().join('')}\n`)
       process.exit(0)
     }
   })
