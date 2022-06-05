f5bf40d277eb340271e5e015e8e98dd98849ae04
Tatyana404 <kot.kate2020@gmail.com>
2022-06-05 15:20:29 +0300

    feat: add function spawnChildProcess

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,21 @@
-export const spawnChildProcess = async (args) => {
-    // Write your code here
-};
+import { spawn } from 'child_process'
+
+export const spawnChildProcess = async args => {
+  const cp = spawn('node', ['./files/script.js', ...args])
+
+  cp.stdout.on('data', data => {
+    console.log(data.toString())
+  })
+
+  process.stdin.pipe(cp.stdin)
+
+  cp.stderr.on('data', err => {
+    console.error(err.toString())
+  })
+
+  cp.on('close', code => {
+    console.log(`Child process exited with code ${code}`)
+  })
+}
+
+await spawnChildProcess(['arg1', 'arg2', 'arg3', 'arg4'])
