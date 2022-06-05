516151bee036b2b557217298ed7f0d185128abf6
Tatyana404 <kot.kate2020@gmail.com>
2022-06-05 21:32:50 +0300

    refactor: cp module refactor

--- src/cp/files/script.js
+++ src/cp/files/script.js
@@ -1,12 +1,14 @@
-const argv = process.argv.slice(2);
+const argv = process.argv.slice(2)
 
-console.log(`Total number of arguments is ${argv.length}`);
-console.log(`Arguments: ${JSON.stringify(argv)}`);
+console.log(`Total number of arguments is ${argv.length}`)
+console.log(`Arguments: ${JSON.stringify(argv)}`)
 
-const echoInput = (chunk) => {
-    const chunkStringified = chunk.toString();
-    if (chunkStringified.includes('CLOSE')) process.exit(0);
-    process.stdout.write(`Received from master process: ${chunk.toString().trim()}`)
-};
+const echoInput = chunk => {
+  if (chunk.toString().includes('CLOSE')) {
+    process.exit(0)
+  }
 
-process.stdin.on('data', echoInput);
+  process.stdout.write(`Received from master process: ${chunk.toString().trim()}`)
+}
+
+process.stdin.on('data', echoInput)
